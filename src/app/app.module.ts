49d554fa115ff526acbfeb6bb1bff714112133b6
sanada shohei <shohei@sanada-no-MacBook-Air.local>
2017-09-23 01:12:08 +0900

    足し算の機能 追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//双方向データバインディングに必要
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
 
@@ -10,9 +13,10 @@ import { CalcComponent } from './calc/calc.component';
     CalcComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CalcComponent]
 })
 export class AppModule { }
--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calc',
+  selector: 'app-root',
   templateUrl: './calc.component.html',
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent implements OnInit {
 
+  val1:number = 10;
+  val2:number = 20;
+  ans:number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(event){
+
+    this.ans = Number(this.val1) + Number(this.val2);
+
   }
 
 }
